ca0f4ef52d6e731cac9cc9ec466ffaac7ca2c62c
saskianadira <saskianadira1@gmail.com>
2024-03-13 21:06:38 +0700

    p5

---
+++ resources/views/latihan5_view.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="/public/images/gambar1.jpg" alt="gambar 1">
+    <img src="/public/images/gambar2.jpg" alt="gambar 2">
+    <h1>Hello, ini adalah warna merah</h1>
+    
+    <link rel="stylesheet" href="/styles/saskia_Style.css">
+
+</body>
+</html>
